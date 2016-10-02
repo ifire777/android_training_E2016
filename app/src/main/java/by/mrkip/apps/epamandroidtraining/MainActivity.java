aa351498641aa654a9dcebdad0e1894de61275e9
ifire777 <kudziev.ip@gmail.com>
2016-10-03 00:45:09 +0300

    hamework 031016: Save instance state example

---
+++ app/src/main/java/by/mrkip/apps/epamandroidtraining/MainActivity.java
@@ -0,0 +1,89 @@
+package by.mrkip.apps.epamandroidtraining;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.DragEvent;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements View.OnTouchListener{
+
+    private String msg="MyAppLogTag";
+private TextView tv;
+    private RelativeLayout cl;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        cl=(RelativeLayout) findViewById(R.id.content_main);
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+         tv=(TextView) findViewById(R.id.mm_txView);
+
+        cl.setOnTouchListener(this);
+       // tv.setOnTouchListener(this);
+
+    }
+
+
+
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+       // Log.d(msg, " event_UP");
+        tv.setX(event.getX());
+        tv.setY(event.getY());
+        return true;
+
+    }
+
+
+    protected class mDragManager  implements View.OnDragListener {
+
+
+        public mDragManager() {
+            Log.d(msg, "ACTION_DROP event1");
+        }
+
+
+        @Override
+        public boolean onDrag(View v, DragEvent event) {
+        return false;
+        }
+    }
+}
+
